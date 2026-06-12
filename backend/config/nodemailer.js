604985f9850c3efc4e451d6f1fcef2049e46e7b9
kansalakshat <kansal.akshat757@gmail.com>
2026-06-12 23:57:34 +0530

    Refactor nodemailer.js to use fetch for email sending

--- backend/config/nodemailer.js
+++ backend/config/nodemailer.js
@@ -1,8 +1,25 @@
-import * as Brevo from '@getbrevo/brevo';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const apiInstance = new Brevo.TransactionalEmailsApi();
-apiInstance.setApiKey(0, process.env.BREVO_API_KEY);
+const sendBrevoEmail = async ({ to, subject, htmlContent }) => {
+    const response = await fetch('https://api.brevo.com/v3/smtp/email', {
+        method: 'POST',
+        headers: {
+            'accept': 'application/json',
+            'api-key': process.env.BREVO_API_KEY,
+            'content-type': 'application/json',
+        },
+        body: JSON.stringify({
+            sender: { email: "kansal.akshat757@gmail.com", name: "CCPS Portal" },
+            to: [{ email: to }],
+            subject,
+            htmlContent,
+        }),
+    });
 
-export default apiInstance;
+    const data = await response.json();
+    if (!response.ok) throw new Error(JSON.stringify(data));
+    return data;
+};
+
+export default sendBrevoEmail;
