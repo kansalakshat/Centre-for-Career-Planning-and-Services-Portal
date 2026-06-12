e7d5bf58a90cde0256c1a5433b9fb36ada1dd923
kansalakshat <kansal.akshat757@gmail.com>
2026-06-12 23:51:08 +0530

    Update nodemailer.js

--- backend/config/nodemailer.js
+++ backend/config/nodemailer.js
@@ -1,8 +1,8 @@
-import * as Brevo from '@getbrevo/brevo';
+import SibApiV3Sdk from '@getbrevo/brevo';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const apiInstance = new Brevo.TransactionalEmailsApi();
+const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
 apiInstance.authentications['apiKey'].apiKey = process.env.BREVO_API_KEY;
 
 export default apiInstance;
