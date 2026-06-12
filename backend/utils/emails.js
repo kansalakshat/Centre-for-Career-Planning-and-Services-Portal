7d87169d4fe356b6761909b4a9fa6ef0a22f4f23
kansalakshat <kansal.akshat757@gmail.com>
2026-06-12 23:15:55 +0530

    brevo

--- backend/utils/emails.js
+++ backend/utils/emails.js
@@ -1,16 +1,17 @@
 import dotenv from "dotenv";
 dotenv.config();
-import resend from "../config/nodemailer.js";
 import { PASSWORD_RESET_REQUEST_TEMPLATE, PASSWORD_RESET_SUCCESS_TEMPLATE, VERIFICATION_EMAIL_TEMPLATE } from "../assets/emailTemplates.js";
+import transporter from "../config/nodemailer.js";
 
 const sendVerificationEmail = async (email, verificationToken) => {
     try {
-        await resend.emails.send({
-            from: 'CCPS Portal <onboarding@resend.dev>',
+        const mailOptions = {
+            from: process.env.SMTP_SENDER_EMAIL,
             to: email,
             subject: 'Verify Your Email',
             html: VERIFICATION_EMAIL_TEMPLATE.replace("{verificationCode}", verificationToken),
-        });
+        }
+        await transporter.sendMail(mailOptions);
         console.log("Verification email sent successfully");
     } catch (error) {
         console.error("Error sending verification email:", error.message);
@@ -19,12 +20,13 @@ const sendVerificationEmail = async (email, verificationToken) => {
 
 const sendPasswordResetEmail = async (email, resetURL) => {
     try {
-        await resend.emails.send({
-            from: 'CCPS Portal <onboarding@resend.dev>',
+        const mailOptions = {
+            from: process.env.SMTP_SENDER_EMAIL,
             to: email,
             subject: 'Reset your password',
             html: PASSWORD_RESET_REQUEST_TEMPLATE.replace("{resetURL}", resetURL),
-        });
+        }
+        await transporter.sendMail(mailOptions);
         console.log("Password reset email sent successfully");
     } catch (error) {
         console.error("Error sending password reset email:", error.message);
@@ -33,12 +35,13 @@ const sendPasswordResetEmail = async (email, resetURL) => {
 
 const sendPasswordResetSuccessEmail = async (email) => {
     try {
-        await resend.emails.send({
-            from: 'CCPS Portal <onboarding@resend.dev>',
+        const mailOptions = {
+            from: process.env.SMTP_SENDER_EMAIL,
             to: email,
             subject: 'Password Reset Successful',
             html: PASSWORD_RESET_SUCCESS_TEMPLATE,
-        });
+        }
+        await transporter.sendMail(mailOptions);
         console.log("Password reset success email sent successfully");
     } catch (error) {
         console.error("Error sending password reset success email:", error.message);
@@ -47,13 +50,13 @@ const sendPasswordResetSuccessEmail = async (email) => {
 
 const sendEmail = async ({ to, subject, html }) => {
     try {
-        await resend.emails.send({
-            from: 'CCPS Portal <onboarding@resend.dev>',
+        const info = await transporter.sendMail({
+            from: `"CCPS Portal" <${process.env.SMTP_SENDER_EMAIL}>`,
             to,
             subject,
             html,
         });
-        console.log("Email sent successfully");
+        console.log("Email sent:", info.response);
     } catch (error) {
         console.error("Error sending email:", error.message);
     }
