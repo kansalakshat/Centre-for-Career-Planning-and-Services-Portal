19b08738a4e4d0e1f140a681994dc8984f4cd398
kansalakshat <kansal.akshat757@gmail.com>
2026-06-12 23:42:04 +0530

    brevohttp

--- backend/config/nodemailer.js
+++ backend/config/nodemailer.js
@@ -1,23 +1,8 @@
-import dotenv from "dotenv";
+import * as Brevo from '@getbrevo/brevo';
+import dotenv from 'dotenv';
 dotenv.config();
-import nodemailer from 'nodemailer';
 
-const transporter = nodemailer.createTransport({
-    host: "smtp-relay.brevo.com",
-    port: 587,
-    secure: false,
-    auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASSWORD,
-    },
-});
+const apiInstance = new Brevo.TransactionalEmailsApi();
+apiInstance.authentications['apiKey'].apiKey = process.env.BREVO_API_KEY;
 
-transporter.verify(function (error, success) {
-    if (error) {
-        console.error("SMTP connection failed:", error);
-    } else {
-        console.log("SMTP server is ready to take our messages");
-    }
-});
-
-export default transporter;
+export default apiInstance;
